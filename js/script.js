53c01fe97661e8f696e631190eebee3b6e886c69
Elliott Hughes-Taylor <ehughestaylor@hotmail.com>
2015-01-27 21:18:42 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+
+$(".button1").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".projects").offset().top},
+        'slow');
+});
+$(".button2").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".introduction").offset().top},
+        'slow');
+});
+$(".carrot2").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".introduction").offset().top},
+        'slow');
+});
+
+$(".button3").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".masthead").offset().top},
+        'slow');
+});
+$(".carrot").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".projects").offset().top},
+        'slow');
+});
+
+
+$(".about").click(function() {
+    $('html,body').animate({
+        scrollTop: $(index.html, ".introduction").offset().top},
+        'slow');
+});
+
